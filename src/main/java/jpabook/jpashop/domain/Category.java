a27fdc81e8e1e3a4001b45c7bb66fb9d115d104f
CJ <choijun0627@konkuk.ac.kr>
2023-10-05 17:07:05 +0900

    <feat> JPA 기본편 프로젝트 section 8
    프로젝트에 지연 로딩, cascade 적용
    
    프로젝트에 엔티티에 @ManyToOne, @OneToOne을 전부 지연로딩으로 설정함
    그리고 Order-Delivery, Order-OrderItem cascade ALL로 해서 Order만
    저장하면 다 저장되게 영속성 전이 설정

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -4,6 +4,8 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.*;
+
 @Entity
 public class Category extends BaseEntity{
     @Id @GeneratedValue
@@ -11,7 +13,7 @@ public class Category extends BaseEntity{
 
     private String name;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name="PARENT_ID")
     private Category parent;
 
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,6 +2,8 @@ package jpabook.jpashop.domain;
 
 import javax.persistence.*;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 public class OrderItem extends BaseEntity{
 
@@ -14,11 +16,11 @@ public class OrderItem extends BaseEntity{
 //    @Column(name="ITEM_ID")
 //    private Long itemId;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "ORDER_ID")
     private Order order;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "ITEM_ID")
     private Item item;
 
