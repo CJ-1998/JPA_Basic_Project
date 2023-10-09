49e866f82e93ec056a8cd49d363ccfe03535c20c
CJ <choijun0627@konkuk.ac.kr>
2023-10-09 23:21:49 +0900

    <feat> JPA 기본편 프로젝트 section 9
    값 타입 실전 예제에 적용
    
    Address라는 값 타입을 만들어서 Member, Delivery에 적용함

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -9,12 +9,46 @@ public class Delivery extends BaseEntity{
     @Id @GeneratedValue
     private Long id;
 
-    private String city;
-    private String street;
-    private String zipcode;
+//    private String city;
+//    private String street;
+//    private String zipcode;
     private DeliveryStatus status;
+
+    @Embedded
+    private Address address;
 
     @OneToOne(mappedBy="delivery", fetch = LAZY)
     private Order order;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public DeliveryStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(DeliveryStatus status) {
+        this.status = status;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
 }
--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -11,9 +11,13 @@ public class Member extends BaseEntity{
     @Column(name="MEMBER_ID")
     private Long id;
     private String name;
-    private String city;
-    private String street;
-    private String zipcode;
+
+    @Embedded
+    private Address address;
+
+//    private String city;
+//    private String street;
+//    private String zipcode;
 
     @OneToMany(mappedBy = "member")
     private List<Order> orders=new ArrayList<>();
@@ -34,27 +38,38 @@ public class Member extends BaseEntity{
         this.name = name;
     }
 
-    public String getCity() {
-        return city;
+    public Address getAddress() {
+        return address;
     }
 
-    public void setCity(String city) {
-        this.city = city;
+    public void setAddress(Address address) {
+        this.address = address;
     }
 
-    public String getStreet() {
-        return street;
+    public List<Order> getOrders() {
+        return orders;
     }
 
-    public void setStreet(String street) {
-        this.street = street;
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
     }
 
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
+    //    public String getCity() {
+//        return city;
+//    }
+//    public void setCity(String city) {
+//        this.city = city;
+//    }
+//    public String getStreet() {
+//        return street;
+//    }
+//    public void setStreet(String street) {
+//        this.street = street;
+//    }
+//    public String getZipcode() {
+//        return zipcode;
+//    }
+//    public void setZipcode(String zipcode) {
+//        this.zipcode = zipcode;
+//    }
 }
